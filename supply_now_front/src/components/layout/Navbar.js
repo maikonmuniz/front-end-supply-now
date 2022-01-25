4c17f7cf10a4adf8ca88e3e3947f5aaf68b741f5
maikonmuniz <maikon.muniz@yahoo.com.br>
2022-01-25 18:42:06 -0300

    login e logout

--- supply_now_front/src/components/layout/Navbar.js
+++ supply_now_front/src/components/layout/Navbar.js
@@ -1,20 +1,38 @@
 import {Link} from 'react-router-dom'
 import styles from './Navbar.module.css'
+import { useContext } from 'react'
+
+//context 
+import {Context} from '../../context/UserContext'
+
 
 function Navbar(){
+
+    const { authenticated, logout } = useContext(Context)
+
     return (
+
         <nav className={styles.navbar}>
             <h1 className={styles.navbar_logo}>Supply Now</h1>
             <ul>
             <li>
                 <Link to="/">Produtos</Link>
             </li>
+            {authenticated ? (
+            <>
+            <li onClick={logout}>
+                <Link to="/"> Sair</Link>
+            </li>
+            </>
+            ) : (
+            <>
             <li>
                 <Link to="/Register">Cadastro</Link>
             </li>
             <li>
                 <Link to="/Login">Login</Link>
             </li>
+            </>)}
             </ul>
         </nav>
     )
--- supply_now_front/src/hooks/useAuth.js
+++ supply_now_front/src/hooks/useAuth.js
@@ -1,7 +1,23 @@
+import {useState, useEffect } from 'react'
 import api from '../utils/api'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function useAuth(){
+
+    const [authenticated, setAuthenticated] = useState(false)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if(token){
+        api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+        setAuthenticated(true)
+        }
+
+    }, [])
+
     async function register(user){
 
         try {
@@ -9,9 +25,44 @@ export default function useAuth(){
                 return response.data
             })
             console.log(data)
+            await authUser(data)
+
         }catch(error){
             console.log(error)
         }
     }
-    return { register }
+
+    async function authUser(data){
+
+        setAuthenticated(true)
+
+        localStorage.setItem('token', JSON.stringify(data.token))
+
+        navigate('/')
+    }
+
+    async function login(user){
+
+        try{
+
+            const data = await api.post('/users/login', user).then((response) => {
+            
+                return response.data
+
+            })
+            await authUser(data)
+        } catch(error){
+
+        }
+        
+    }
+
+    function logout(){
+        setAuthenticated(false)
+        localStorage.removeItem('token')
+        api.defaults.headers.Authorization = undefined
+        navigate('/')
+    } 
+
+    return {authenticated, register, logout, login }
  }
